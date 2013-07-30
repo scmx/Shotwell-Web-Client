7cb567663b4413c5bb3efed6f92ff6530b440749
Albert Arvidsson <albert.arvidsson@gmail.com>
2013-07-30 17:03:45 +0200

    Remove everything except readme

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ShotwellWebClient::Application.config.secret_token = '13596c0ccabbdc15587f83fc84906830626830d6d51ebd860855c4debb95be8f9ce68e6dfa07d3b6e6967de6b9eb2bc74e5c69d8e733e4ea6e5a8c2703faea86'
