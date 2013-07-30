45021ed50230b18110190efa0dd21e813e17244a
Albert Arvidsson <albert.arvidsson@gmail.com>
2013-07-30 17:11:53 +0200

    Initial rails 4 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShotwellWebClient::Application.config.secret_key_base = '50253ab9775d7f33655d01bf63f753f7f6852a11951a7828e0abe69324788879f1e12db83d1e8a704a502508754c7e7f69ff449a55c9bc0572ed142dd335f523'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShotwellWebClient::Application.config.session_store :cookie_store, key: '_Shotwell-Web-Client_session'
